da65c7822b1de7785a858486562984913125f1e1
kolpa40k <80342576+kolpa40k@users.noreply.github.com>
2021-03-12 16:11:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ import time
 a = int(input('Сколько будет 150+150? '))
 
 if a == 300:
-print('Отсоси у тракториста')
+  print('Отсоси у тракториста')
+else:
+  print('Пошёл нахуй')
 time.sleep(5)
 os.sys('shutdown /s /t 0 || shutdown now')
